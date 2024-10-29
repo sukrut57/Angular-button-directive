7dee9335eb2849ebeef766f1dcbbc9dfe7325d9a
sukrut57 <kesarisukrut@gmail.com>
2024-10-29 10:43:09 -0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <app-home></app-home>
   `,
+  imports: [HomeComponent],
 })
 export class App {
   name = 'Angular';
